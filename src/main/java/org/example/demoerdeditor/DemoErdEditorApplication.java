5f5b2fc7442d53f6b8d6fc416a89308c73ddccf5
handsome-tiger-789 <tropical7890@gmail.com>
2026-02-03 14:34:13 +0900

    feat(create): erd editor 프로젝트 생성
    데모 프로젝트 생성

---
+++ src/main/java/org/example/demoerdeditor/DemoErdEditorApplication.java
@@ -0,0 +1,13 @@
+package org.example.demoerdeditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoErdEditorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoErdEditorApplication.class, args);
+    }
+
+}
